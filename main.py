76fa4f51843315a92670f1292fe6795bb02420a1
aistestonciute <72549610+aistestonciute@users.noreply.github.com>
2023-11-13 18:35:09 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,164 +1,130 @@
-from neo4j import GraphDatabase
-import pandas as pd
-from pandas import DataFrame
+import pymongo
+from pprint import pprint
+import Book  # Importing Book module, assuming it contains the Book class
+import Review  # Importing Review module, assuming it contains the Review class
+from pymongo import MongoClient
 
-class Neo4jConnection:
+try:
+    # Attempting to establish a connection to the MongoDB server
+    client = MongoClient('localhost', 27017)
+    print("\nSuccessfully connected!")
+except:
+    # If connection fails, print an error message
+    print("\nConnection failed.")
 
-    def __init__(self, uri, user, pwd):
-        self.__uri = uri
-        self.__user = user
-        self.__pwd = pwd
-        self.__driver = None
-        try:
-            self.__driver = GraphDatabase.driver(self.__uri, auth=(self.__user, self.__pwd))
-        except Exception as e:
-            print("Failed to create the driver:", e)
+# Accessing the 'main' database
+db = client.main
+books = db.book  # Assuming there's a collection named 'book' in the 'main' database
+reviews = db.review  # Assuming there's a collection named 'review' in the 'main' database
 
-    def close(self):
-        if self.__driver is not None:
-            self.__driver.close()
+books.drop()
+reviews.drop()
 
-    def query(self, query, db=None):
-        assert self.__driver is not None, "Driver not initialized!"
-        session = None
-        response = None
-        try:
-            session = self.__driver.session(database=db) if db is not None else self.__driver.session()
-            response = list(session.run(query))
-        except Exception as e:
-            print("Query failed:", e)
-        finally:
-            if session is not None:
-                session.close()
-        return response
-conn = Neo4jConnection(uri="bolt://localhost:11004", user="user", pwd="0000")
+# Function to insert data into the 'book' and 'review' collections
+def insertData():
+    books.insert_many([Book.b1, Book.b2, Book.b3, Book.b4, Book.b5, Book.b6])
+    reviews.insert_many([Review.r1, Review.r2, Review.r3, Review.r4, Review.r5, Review.r6, Review.r7, Review.r8, Review.r9, Review.r10])
 
-conn.query("""MATCH (n) DETACH DELETE n""", db = 'lab4')
+# Function to display the names of all collections in the 'main' database
+def showCollections():
+    print("\nPrinting information about collections: ")
+    print(db.list_collection_names())
 
-def CreateDB():
-    query = """ CREATE 
-    (b2:Bus {name: "b2", route: "Klaipeda-Siauliai-Radviliskis-Vilnius"}),
-    (b3:Bus {name: "b3", route: "Klaipeda-Kaunas"}),
-    (b4:Bus {name: "b4", route: "Kaunas-Vilnius"}),
-    (b5:Bus {name: "b5", route: "Siauliai-Panevezys-Vilnius"}),
+def showBooks():
+    print ("\nPrinting information about books:")
+    cursor = books.find({})
+    for doc in cursor:
+        pprint(doc, sort_dicts=False)
 
 
-    (c1:City {name: "Siauliai"}),
-    (c3:City {name: "Klaipeda"}),
-    (c4:City {name: "Radviliskis"}),
-    (c5:City {name: "Vilnius"}),
-    (c6:City {name: "Kaunas"}),
-    (c7:City {name: "Panevezys"}),
+def showReviews():
+    print("\nPrinting reviews:")
+    cursor = reviews.find({})
+    for doc in cursor:
+        pprint(doc, sort_dicts=False)
 
 
-    (b2)-[:TRAVEL_TO]->(c3),
-    (b2)-[:TRAVEL_TO]->(c1),
-    (b2)-[:TRAVEL_TO]->(c4),
-    (b2)-[:TRAVEL_TO]->(c5),
+def checkBook(title):
+    if books.count_documents({"title": title}) > 0:
+        return
+    else:
+        # If the book doesn't exist, raise a ValueError
+        raise ValueError("The book does not exist")
 
-    (b3)-[:TRAVEL_TO]->(c3),
-    (b3)-[:TRAVEL_TO]->(c5),
+# Function to display information about the author of a book
+def authorInformation(title):
+    checkBook(title)
+    query = {"title": title}
+    doc = books.find(query, {"author"})
+    print("\nPrinting information about the author of " + title + ":")
+    for i in doc:
+        pprint(i, sort_dicts=False)
 
-    (b4)-[:TRAVEL_TO]->(c6),
-    (b4)-[:TRAVEL_TO]->(c5),
+# Function to display information about the count of reviews per user
+def reviewCount():
+    print("\nInformation about users' review count: \n")
+   
+    pipeline = [{"$group": {"_id": "$user", "countReviews": {"$count": {}}}}]
+    rows = list(reviews.aggregate(pipeline))
+    print(rows)
 
-    (b5)-[:TRAVEL_TO]->(c1),
-    (b5)-[:TRAVEL_TO]->(c7),
-    (b5)-[:TRAVEL_TO]->(c5),
+# Function to display information about the count of reviews per user using MapReduce
+def reviewCount_MapReduce():
+    print("\nInformation about users' review count using map reduce: \n")
+    mapF = "function() { emit(this.user, 1) }"
+    reduceF = "function(k, v) { return Array.sum(v)}"
+    result = db.command(
+        'mapReduce',
+        'review',
+        map=mapF,
+        reduce=reduceF,
 
+        out={'inline': 1}
+    )
+    print(result)
 
-    (c1)-[:ROAD {cost: 5}]->(c4),
-    (c1)-[:ROAD {cost: 5.5}]->(c7),
-    (c1)-[:ROAD {cost: 7}]->(c3),
+# Function to display review count information and call the MapReduce function
+def reviewCountPrint():
+    reviewCount()
+    reviewCount_MapReduce()
 
-    (c3)-[:ROAD {cost: 7}]->(c1),
-    (c3)-[:ROAD {cost: 9}]->(c6),
-
-    (c6)-[:ROAD {cost: 7}]->(c3),
-    (c6)-[:ROAD {cost: 4.5}]->(c5),
-
-    (c5)-[:ROAD {cost: 4.5}]->(c6),
-    (c5)-[:ROAD {cost: 6}]->(c7),
-
-    (c7)-[:ROAD {cost: 5.5}]->(c1),
-    (c7)-[:ROAD {cost: 6}]->(c5),
-
-    (c4)-[:ROAD {cost: 7.5}]->(c5)
-    """
-    conn.query(query, db = "lab4")
-
-def FindRoute(bus):
-    findBus = (f"MATCH (b:Bus) WHERE b.name = '{bus}' RETURN b.name, b.route")
-    print(conn.query(findBus, db = "lab4"))
-
-def FindStops(stop):
-    findStops = (f"MATCH stops = (b:Bus WHERE b.name = '{stop}')-[:TRAVEL_TO]-(city) RETURN city.name as cityName")
-    print(conn.query(findStops, db = "lab4"))
-
-def FindAllRoutes(a, b):
-    findAllRoutes = (f"MATCH p = (a:City WHERE a.name = '{a}')-[:ROAD*]-(b:City WHERE b.name = '{b}') unwind nodes(p) as node with p, collect(node.name) as roads RETURN Distinct roads")
-    dtf_data = DataFrame([dict(_) for _ in conn.query(findAllRoutes, db = "lab4")])
-    print(dtf_data["roads"].values)
-
-def FindCheapestTrip(a, b):
-    findCheapestTrip =(f"MATCH p = (a:City WHERE a.name = '{a}')-[:ROAD*]-(b:City WHERE b.name = '{b}') "
-                           f"unwind nodes(p) as node with p, collect(node.name) as paths "
-                           f"RETURN Distinct paths, REDUCE(s = 0, d IN RELATIONSHIPS(p) | s + d.cost) AS cost "
-                           f"ORDER BY cost ASC LIMIT 1")
-    dtf_data = DataFrame([dict(_) for _ in conn.query(findCheapestTrip, db = "lab4")])
-    print(dtf_data.get(["paths", "cost"]))
-
-def NumberOfRoutes(a, b):
-    numberOfRoutes = (f"MATCH p = (a:City WHERE a.name = '{a}')-[:ROAD*]-(b:City WHERE b.name = '{b}') "
-                           f"unwind nodes(p) as node with p, collect(node.name) as paths "
-                           f"RETURN Distinct paths, REDUCE(s = 0, d IN RELATIONSHIPS(p) | s + d.cost) AS cost "
-                           f"ORDER BY cost ASC")
-    dtf_data = DataFrame([dict(_) for _ in conn.query(numberOfRoutes, db = "lab4")])
-    print(dtf_data.get(["paths", "cost"]))
-
+# Main function to execute the program
 def main():
-    CreateDB()
+    insertData()
     while True:
-        print("""\nActions:
-    1 - Find route
-    2 - Find stops
-    3 - Find all routes
-    4 - Find cheapest trip
-    5 - Find number of routes
-    6 - Exit
-    """)
+        print("""
+        Select the action:
+        1 - Show collections
+        2 - Show books' data
+        3 - Show reviews' data
+        4 - Show information about book's author
+        5 - Show number of reviews written by user
+        6 - Exit
+        """)
 
-        action = input("Choose action: ")
-        if (action == '1'):
-            bus = input("Enter bus number: ")
-            FindRoute(bus)
+        action = input('Enter the number of the action: ')
+
+        if(action == '1'):
+            showCollections()
 
         elif (action == '2'):
-            bus = input("Enter bus number: ")
-            FindStops(bus)
+            showBooks()
 
-        elif (action == '3'):
-            a = input("Enter start destination: ")
-            b = input("Enter end destination: ")
-            FindAllRoutes(a, b)
+        elif(action == '3'):
+            showReviews()
 
-        elif (action == '4'):
-            a = input("Enter start destination: ")
-            b = input("Enter end destination: ")
-            FindCheapestTrip(a, b)
+        elif(action == '4'):
+            title = input("Enter the title of the book: ")
+            authorInformation(title)
 
-        elif (action == '5'):
-            a = input("Enter start destination: ")
-            b = input("Enter end destination: ")
-            NumberOfRoutes(a, b)
+        elif(action == '5'):
+            reviewCountPrint()
 
-        elif (action == '6'):
-            break;
+        elif(action == '6'):
+            print("The program is closing...")
+            break
 
-        else:
-            print("Action does not exist")
-
-    conn.close()
+        else: raise ValueError("The action number is incorrect")
 
 if __name__ == '__main__':
     main()
